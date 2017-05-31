1585deb2b6d0c4f3127e895120d7ea69a06d4c6e
si shen <shensilasg@gmail.com>
2017-05-31 15:25:54 -0600

    verif_sfcobs for subdomains (3KM)

---
+++ subdomains/verif_sfcobs.input.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+
+@DOM_NAME=("xinjiang", "huabei", "dongbei");
+@DOM_ID=(3,4,5);
+@OBS_DOM_ID=(3,3,3); #all use dom3's thined_obs
+@WRF_DOM_ID=(2,2,2); #all use wrf_d02's file
+@DOM_LON1=(73, 100, 110);
+@DOM_LON2=(100,123, 135);
+@DOM_LAT1=(35, 32,  42);
+@DOM_LAT2=(50, 44,  54);
+
+
