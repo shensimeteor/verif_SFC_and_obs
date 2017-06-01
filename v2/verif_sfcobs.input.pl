efbacbd6ac6031c7f5625ef04ec5c41a73b246d3
si shen <shensilasg@gmail.com>
2017-06-01 13:00:27 -0600

    first create v2, merge subdomain together, driver add parallel run

---
+++ v2/verif_sfcobs.input.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+#domain
+@DOM_NAME=("domain2", "xinjiang", "huabei", "dongbei");
+@DOM_ID=(2,3,4,5);
+@OBS_DOM_ID=(2,2,2,2); #all use dom3's thined_obs
+@WRF_DOM_ID=(2,2,2,2); #all use wrf_d02's file
+@DOM_LON1=(-1, 73, 100, 110); #-1 means no zoom
+@DOM_LON2=(-1, 100,123, 135);
+@DOM_LAT1=(-1, 35, 32,  42);
+@DOM_LAT2=(-1, 50, 44,  54);
